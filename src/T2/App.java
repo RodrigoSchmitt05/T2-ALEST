bd51b17512197860909159fe883f33283db4bf6b
Rodrigo Schmitt de Almeida <rodrigoschmitt0508@gmail.com>
2025-06-11 21:46:10 -0300

    commit1

---
+++ src/T2/App.java
@@ -0,0 +1,11 @@
+package T2;
+public class App {
+    public static void main(String[] args) {
+        App app = new App();
+        app.rodarAplicacao();
+    }
+    public void rodarAplicacao() {
+        //aqui deve ser feito o trabalho
+
+    }
+}
